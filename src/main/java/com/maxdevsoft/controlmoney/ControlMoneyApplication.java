91da605799d7e2591817a0c1bcab45fcd07653c6
MaxDevSoft <max.s.taveira@gmail.com>
2023-08-05 10:00:02 -0300

    first commit

---
+++ src/main/java/com/maxdevsoft/controlmoney/ControlMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.maxdevsoft.controlmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxdevsoft/controlmoney/ControlMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxdevsoft.controlmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
