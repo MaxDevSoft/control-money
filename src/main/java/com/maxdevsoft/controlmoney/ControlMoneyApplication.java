1b5c59d6c99b39d516a6fc7c0980ea02b0bc1286
MaxDevSoft <max.s.taveira@gmail.com>
2023-08-12 20:30:39 -0300

    hello world

--- src/main/java/com/maxdevsoft/controlmoney/ControlMoneyApplication.java
+++ src/main/java/com/maxdevsoft/controlmoney/ControlMoneyApplication.java
@@ -2,12 +2,20 @@ package com.maxdevsoft.controlmoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // classe do tipo controller
 public class ControlMoneyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlMoneyApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello(){
+
+		return "Hello word";
+	}
 }
