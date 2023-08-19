7646654afd1be545cb9addf1e455f4a8e3142884
MaxDevSoft <maxdevelops2020@gmail.com>
2023-08-19 13:12:46 -0300

    Update Controller

---
+++ src/main/java/com/maxdevsoft/controlmoney/controller/ControlMoneyController.java
@@ -0,0 +1,11 @@
+package com.maxdevsoft.controlmoney.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControlMoneyController {
+
+    
+
+    
+}
